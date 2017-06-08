fdcc60773b51ffeea4f3ac18c33e504639e16709
Don Cooper <don_cooper@hotmail.com>
2017-06-08 16:54:59 -0400

    Add new contact working

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,20 +1,42 @@
 class ContactsController < ApplicationController
 
-  def contacts
+  def index
     @contacts = Contact.all
     render :contacts
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+    render :contact
   end
 
   def about
     render :about
   end
 
-  def add_contact
+  def new
+    @contact = Contact.new
     render :add_contact
   end
 
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to contacts_path
+    else
+      render :add_contact
+    end
+  end
+
   def delete_contact
+    @contact = Contact.find(params[:id])
     render :delete_contact
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    render :contacts
   end
 
   def not_found
@@ -25,8 +47,17 @@ class ContactsController < ApplicationController
     render :search_results
   end
 
-  def update_contact
-    render :update_contact
+  def update
+    @meme = Meme.find(params[:id])
+    if @meme.update_attributes(meme_params)
+      redirect_to "/memes/#{@meme.id}"
+    else
+      render :update_contact
+    end
+  end
+
+  def contact_params
+    { first_name: params[:contact][:first_name], last_name: params[:contact][:last_name], email: params[:email], note: params[:note] }
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  root 'contacts#contacts'
+  root 'contacts#index'
   resources :contacts
 
   get '/about' => 'contacts#about'
-  get '/add_contact' => 'contacts#add_contact'
+
+  get '/add_contact' => 'contacts#new'
+  post '/add_contact' => 'contacts#create'
+
   get '/contact' => 'contacts#contact'
   get '/contacts' => 'contacts#contacts'
+
   get '/delete_contact' => 'contacts#delete_contact'
   get '/not_found' => 'contacts#not_found'
   get '/search_results' => 'contacts#search_results'
